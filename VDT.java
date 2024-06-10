26ef15870938aea0183646322470deb5f74b7b60
Kusumayadav19 <kushhhyadav19@gmail.com>
2024-06-10 12:58:28 +0100

    #day1 of Java

--- VDT.java
+++ VDT.java
@@ -1,5 +1,17 @@
 package FirstRepo;
 
+import java.util.*;
+
 public class VDT {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Sum :" + (a + b));
+        System.out.println("Product :" + (a * b));
+        System.out.println("Sub :" + (a - b));
+        System.out.println("Division: " + (a / b));
+        sc.close();
+    }
     
 }
